d4ecf799ed63781ec38240706152eaf87b2c5221
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2023-05-04 01:44:00 -0400

    fix: CodeSmell

--- src/main/java/TINGESO/monolitico/repositories/PagoRepository.java
+++
@@ -1,9 +0,0 @@
-package tingeso.monolitico.repositories;
-
-import tingeso.monolitico.entities.PagoEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface PagoRepository extends JpaRepository<PagoEntity, Long>{
-}
